620ac0d89af8814c35c7e9ba6549256d20219451
t1entv <tientranvan19052004@gmail.com>
2025-03-10 15:06:09 +0700

    int commit

--- src/main/java/org/tientv/controller/AdminController.java
+++ src/main/java/org/tientv/controller/AdminController.java
@@ -6,5 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 
-
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
 }
